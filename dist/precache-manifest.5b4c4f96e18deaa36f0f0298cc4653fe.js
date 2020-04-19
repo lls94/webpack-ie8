8510d1c12e78218a2bdccda1c7d1ad358a0cff7e
lls94 <1173967838@qq.com>
2020-04-19 16:54:43 +0800

    modify

---
+++ dist/precache-manifest.5b4c4f96e18deaa36f0f0298cc4653fe.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "739071116cec21ecd908",
+    "url": "test.css"
+  },
+  {
+    "revision": "31ecc0af7cfc0a24ea7537007f25fa79",
+    "url": "test.html"
+  },
+  {
+    "revision": "739071116cec21ecd908",
+    "url": "test.js?v=661cb5"
+  }
+]);
--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -4,23 +4,31 @@ let {
     merge,
     webpack,
     ManifestPlugin,
-    BundleAnalyzerPlugin
-} = require("./exports");
+    BundleAnalyzerPlugin,
+} = require('./exports')
 
 let prodConfig = {
-    mode: "production",
+    mode: 'production',
     plugins: [
         new webpack.DefinePlugin({
-            "process.env.NODE_ENV": JSON.stringify("production")
+            'process.env.NODE_ENV': JSON.stringify('production'),
         }),
         new ManifestPlugin({
-            writeToFileEmit: true
+            writeToFileEmit: true,
         }),
         new BundleAnalyzerPlugin({
             analyzerPort: 1994,
-            defaultSizes: 'gzip'
-        })
-    ]
-};
+            defaultSizes: 'gzip',
+        }),
+        new webpack.BannerPlugin({
+            banner: `hash:[hash], chunkhash:[chunkhash], time:${new Date().toLocaleDateString()}`, // 其值为字符串，将作为注释存在  name:[name], filebase:[filebase], query:[query], file:[file], 
+            raw: false, // 如果值为 true，将直出，不会被作为注释
+            entryOnly: false, // 如果值为 true，将只在入口 chunks 文件中添加
+            // test: string | RegExp | Array,
+            // include: string | RegExp | Array,
+            // exclude: string | RegExp | Array
+        }),
+    ],
+}
 
-module.exports = smp.wrap(merge(require("./webpack.common"), prodConfig));
+module.exports = smp.wrap(merge(prodConfig, require('./webpack.common')))
